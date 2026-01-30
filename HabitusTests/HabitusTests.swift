0a3c64ccdbeb0d1e44f61dd299ac0d74f3a16a19
GeoBangkok <42850725+GeoBangkok@users.noreply.github.com>
2026-01-29 23:18:41 -0500

    Initial Commit

---
+++ HabitusTests/HabitusTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitusTests.swift
+//  HabitusTests
+//
+//  Created by standard on 1/29/26.
+//
+
+import Testing
+@testable import Habitus
+
+struct HabitusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
